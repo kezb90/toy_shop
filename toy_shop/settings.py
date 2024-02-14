d6cb9f977e992118cfa47cf10e8eb23220fbeff3
kezb90 <m90.rahmati@gmail.com>
2024-02-14 10:37:31 +0330

    update settings.py for staticfiles config

--- toy_shop/settings.py
+++ toy_shop/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+# Set global static directoy
+STATICFILES_DIRS = [
+    BASE_DIR / 'globalstaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
