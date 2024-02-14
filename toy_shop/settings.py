968886bf4684c01cb13f675a1824c92dcc7d5612
kezb90 <m90.rahmati@gmail.com>
2024-02-14 11:29:33 +0330

    config settings.py for static files and media files

--- toy_shop/settings.py
+++ toy_shop/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'globalstaticfiles'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
