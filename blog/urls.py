cab3ef622345bc63673e9b087362bba8d32b1395
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-16 00:40:48 +0330

    update for CategoryViewSet router

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from .views import main, about_us, comment_view, category_posts_view, post_detail
+from rest_framework import routers
+from .views import CategoryViewSet
 
+category_router = routers.DefaultRouter()
+category_router.register('', CategoryViewSet, )
 
 urlpatterns = [
+    path("category", include(category_router.urls), name='category'),
     path("", main, name="main"),
     path("blog/", main, name="blog"),
     path("about_us/", about_us, name="about_us"),
     path("comment/<int:post_id>", comment_view, name="comment"),
-    path("category/<int:category_id>/", category_posts_view, name="category_posts"),
+    path("category/<int:category_id>/",
+         category_posts_view, name="category_posts"),
     path("post/<int:post_id>/", post_detail, name="post_detail"),
 ]
