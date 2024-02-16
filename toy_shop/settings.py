c6cddd73516537b87ebfb14ee508623d68abab28
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-16 13:58:48 +0330

    update settings.py ( add media_root )

--- toy_shop/settings.py
+++ toy_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,8 +124,9 @@ STATIC_ROOT = BASE_DIR / "productionfiles"
 # Set global static directoy
 STATICFILES_DIRS = [BASE_DIR / "globalstaticfiles"]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
