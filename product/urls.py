6e415f200f50061b701cdac6ab2012bebc4c4176
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-17 22:00:03 +0330

    handle the product list(view,url,template)

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import product_list
 
 
 urlpatterns = [
-    
+    path('product_list/', product_list, name='product-list')
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Category, Product
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request,'product_list.html', context)
