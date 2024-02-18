61b8f476416e37077678f06df9133de99c8dc2bb
kezb90 <m90.rahmati@gmail.com>
2024-02-18 15:13:01 +0330

    add app_name field

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import product_list
 
-
+app_name = 'product'
 urlpatterns = [
     path('product_list/', product_list, name='product-list')
 ]
--- shopping_basket/views.py
+++ shopping_basket/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .cart import Cart
 from product.models import Product
+
 
 def add_to_cart(request, product_id):
     cart = Cart(request)
     product = Product.objects.get(pk=product_id)
     cart.add_product(product_id, quantity=1)
-    return redirect('product_list')  # Redirect to your product list or wherever you want after adding to the cart
+    return redirect("shopping_basket:view_cart")  # Redirect to your product list or wherever you want after adding to the cart
+
 
 def view_cart(request):
     cart = Cart(request)
@@ -16,7 +17,11 @@ def view_cart(request):
 
     for product_id, item in cart.cart.items():
         product = Product.objects.get(pk=product_id)
-        total_price += product.price * item['quantity']
-        cart_items.append({'product': product, 'quantity': item['quantity']})
+        total_price += product.price * item["quantity"]
+        cart_items.append({"product": product, "quantity": item["quantity"]})
 
-    return render(request, 'view_cart.html', {'cart_items': cart_items, 'total_price': total_price})
+    return render(
+        request,
+        "view_cart.html",
+        {"cart_items": cart_items, "total_price": total_price},
+    )
