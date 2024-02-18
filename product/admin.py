4ca0de802e0ce874a8bfd840c634ba5acef617b6
kezb90 <m90.rahmati@gmail.com>
2024-02-18 13:59:40 +0330

    update admin.py app:product

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import register
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+@register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "price", "stock", "created_at")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "description")
+
+@register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "description")
