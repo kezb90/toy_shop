7d055e51883fb2d38deb9936ed004a0b17ceacdf
kezb90 <m90.rahmati@gmail.com>
2024-02-20 10:44:43 +0330

    update model of product and its admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,16 +1,32 @@
 from django.contrib import admin
 from django.contrib.admin import register
-from .models import Product, Category
+from .models import Product, Category, Price, ProductImage
 
 
 @register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description", "price", "stock", "created_at")
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "is_active",
+        "price",
+        "stock",
+        "created_at",
+    )
     list_display_links = ("id", "name")
     search_fields = ("name", "description")
+
 
 @register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "description")
     list_display_links = ("id", "name")
     search_fields = ("name", "description")
+
+
+@register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "is_active", "amount", "created_at")
+    list_display_links = ("id", "product")
+    search_fields = ("product", "ammount")
--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from blog.models import MyBaseModel
-
+from django.db.models import F, Max
 # Create your models here.
 
 
@@ -16,14 +16,29 @@ class Category(MyBaseModel):
 class Product(MyBaseModel):
     name = models.CharField(max_length=255)
     description = RichTextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
+    @property
+    def price(self):
+        last_price = self.prices.aggregate(last_created=Max('created_at'))['last_created']
+        if last_price:
+            return self.prices.filter(created_at=last_price).first().amount
+        return None
+        
 
-
+class Price(MyBaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='prices')
+    amount = models.PositiveIntegerField(null=False, blank=False)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.product.name
+    
 class ProductImage(MyBaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="product_images/")
