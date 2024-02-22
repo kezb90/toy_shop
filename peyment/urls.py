640d1330b2cf94fb607a9ef4e460e4c2bf89bf9a
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-22 16:46:43 +0330

    update related to peyment page

--- peyment/urls.py
+++ peyment/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from .views import peyment
 
 app_name = "peyment"
-urlpatterns = []
+urlpatterns = [
+    path('peyment_page/', peyment, name='payment-page'),
+]
--- peyment/views.py
+++ peyment/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def peyment(request):
+    context={}
+    return render(request,'peyment.html',context)
