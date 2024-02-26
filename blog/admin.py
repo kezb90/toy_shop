df3c140b8fceed9d0e9bc9479ccd11459ef74ea4
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-26 23:43:15 +0330

    update models and templates

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,14 @@ from django.contrib.admin import register
 from .models import Post, Category, Comment, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
 @register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = (
@@ -21,9 +29,11 @@ class ImageAdmin(admin.ModelAdmin):
 
 @register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "body", "is_active", "created_at", "updated_at")
+    list_display = ("id", "title", "body", "is_active",
+                    "created_at", "updated_at")
     list_display_links = ("id", "title")
     search_fields = ("title",)
+    inlines = [CommentInline]
 
 
 @register(Category)
--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,10 @@ from ckeditor.fields import RichTextField
 
 class MyBaseModel(models.Model):
     is_active = models.BooleanField(verbose_name="Is active", default=False)
-    created_at = models.DateTimeField(verbose_name="date created", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="date updated", auto_now=True)
+    created_at = models.DateTimeField(
+        verbose_name="date created", auto_now_add=True)
+    updated_at = models.DateTimeField(
+        verbose_name="date updated", auto_now=True)
 
     class Meta:
         abstract = True
@@ -23,16 +25,17 @@ class Category(MyBaseModel):
     title = models.CharField(
         max_length=250, null=False, blank=False, verbose_name="title"
     )
-    description = RichTextField(null=False, blank=False, verbose_name="description")
+    description = RichTextField(
+        null=False, blank=False, verbose_name="description")
 
     def __str__(self):
         return self.title
 
 
 class Post(MyBaseModel):
+    categories = models.ManyToManyField("Category", related_name="posts")
     title = models.CharField(max_length=250, null=False, blank=False)
     body = RichTextField()
-    categories = models.ManyToManyField("Category", related_name="posts")
 
     def __str__(self):
         return self.title
@@ -47,19 +50,34 @@ class Comment(MyBaseModel):
         return self.author
 
 
-class Image(MyBaseModel):
+class Video(MyBaseModel):
+    post = models.ForeignKey(
+        "Post", on_delete=models.CASCADE, related_name="videos")
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="media_gallery/")
-    description = models.TextField(blank=True)
+    video_file = models.FileField(upload_to="blog_videos/")
+    description = RichTextField(blank=True)
 
     def __str__(self):
         return self.title
 
 
-class PostImage(MyBaseModel):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="Blog_images/")
-    alt = models.CharField(max_length=150, null=True, blank=True)
+class Image(MyBaseModel):
+    post = models.ForeignKey(
+        "Post", on_delete=models.CASCADE, related_name="images")
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="blog_images/")
+    description = RichTextField(blank=True)
 
     def __str__(self):
-        return self.alt
+        return self.title
+
+
+class Audio(MyBaseModel):
+    post = models.ForeignKey(
+        "Post", on_delete=models.CASCADE, related_name="audios")
+    title = models.CharField(max_length=255)
+    audio_file = models.FileField(upload_to="blog_audios/")
+    description = RichTextField(blank=True)
+
+    def __str__(self):
+        return self.title
