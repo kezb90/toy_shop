86af0bf170488c73b9b727e76f699b36008ed90c
kezb90 <m90.rahmati@gmail.com>
2024-02-26 10:46:07 +0330

    create view for submit a order and its url and related templated

--- peyment/urls.py
+++ peyment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import peyment, resault
+from .views import peyment, resault, create_order
 
 app_name = "peyment"
 urlpatterns = [
     path("peyment_page/", peyment, name="payment-page"),
     path("peyment_resault/<int:is_paid>/", resault, name="resault"),
+    path("create_order/", create_order, name="create-order"),
 ]
--- peyment/views.py
+++ peyment/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from shopping_basket.cart import Cart
 from product.models import Product
+from .models import Order, OrderItem
+from product.models import Product
+from shopping_basket.cart import Cart
 
 # Create your views here.
 
 
 def peyment(request):
+
     cart = Cart(request)
     cart_items = []
     total_price = 0
@@ -42,3 +46,36 @@ def resault(request, is_paid):
     else:
         context = {"status_code": 0}
         return render(request, "resault.html", context)
+
+
+def create_order(request):
+    if request.method == "GET":
+        # Get or create the user's cart
+        cart = Cart(request)
+        # Create order
+        order = Order.objects.create(
+            user=request.user,  # Assuming you have authentication
+            status="pending",  # Set the appropriate status
+        )
+        # Save the order
+        order.save()
+        for product_id, item in cart.cart.items():
+            # Retrieve the product
+            product = Product.objects.get(pk=product_id)
+            quantity = item["quantity"]
+
+            
+            # Create OrderItem
+            order_item = OrderItem.objects.create(
+                order=order,  # You will update this with the actual order instance
+                product=product,
+                quantity=quantity,
+            )
+
+            # You may want to update additional fields such as shipping address, payment method, etc.
+            # Save the order_item
+            order_item.save()
+
+            # Clear the cart
+        cart.clear()
+        return redirect("peyment:payment-page")  # Redirect to a peyment page
